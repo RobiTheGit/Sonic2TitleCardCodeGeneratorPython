79824cfc01e7615f243fcf100c11e8cd39d54e2f
RobiTheGit <94720060+RobiTheGit@users.noreply.github.com>
2022-11-27 09:27:32 -0700

    calculated spacing

--- s2tcg.py
+++ s2tcg.py
@@ -57,7 +57,11 @@ def gen():
 -2:'0060',
 -1:'0070',
 0:'0080'
-}   
+}  #replace with calculating positions
+    pos_br = 65520
+    pos_inc = 16 #$10, after M or W, 24/$18, after I, 8/$8
+    cur_pos = 65428 #starts with the starting position
+    after0 = False
     
     letter = 0
     current = 1500
@@ -67,6 +71,14 @@ def gen():
     text = str(input('Level Name > '))
     ntext = text.replace(" ", "")
     hexi = hex(len(ntext))
+    if len(text) >= 10:
+        cur_pos = 65428
+    elif len(text) <= 9 and len(text) > 5:
+        cur_pos = 0
+        after0 = True            
+    else:
+        cur_pos = 28 
+        after0 = True        
     print('In Obj34_MapUnc_147BA Put')
     if export == True:
         f.write(f'In Obj34_MapUnc_147BA Put\n')
@@ -79,23 +91,43 @@ def gen():
     charlistcode = []
     for char in text:
         code.append(char.lower())   
-        afterI = False  
+        afterI = False 
+        afterM = False 
         pos = -(len(code))
     if len(char) <= 15:
         for char in code:
-            pos += 1
-            char = char.lower()
-            XPOS = XPOSLIST.get(pos)
             if letter >= 1:
                 increment = 4
             if afterI == True:
                 increment = 2
                 twopinc = 1
+                pos_inc = 8
                 if afterIcount == 0:
                     increment = 4
                     twopinc = 2
+                    pos_inc = 16
                 else:
-                    afterIcount -= 1   
+                    afterIcount -= 1  
+            if afterM == True:
+                pos_inc = 24                    
+                if afterMcount == 0:
+                    pos_inc = 16
+                else:
+                    afterMcount -= 1 
+                    
+            if cur_pos <= pos_br:
+                cur_pos += pos_inc
+                char = char.lower()
+                xpos_b = hex(cur_pos)
+                if after0 == False:
+                    XPOS = xpos_b.replace("0x", "").upper()
+                else:
+                    XPOS = xpos_b.replace("0x", "00").upper()
+            else:
+                cur_pos = 0
+                XPOS = '0000'
+                after0 = True            
+                                        
             if char in charlist:
                 x = charlist.index(char) 
                 print(charlistcode[x],f'${XPOS} ; {char.upper()}')
@@ -121,7 +153,10 @@ def gen():
                     twopcurrent = twopresult
                     if char == 'i':
                        afterI = True 
-                       afterIcount = 2                   
+                       afterIcount = 2
+                    if char == 'm':
+                       afterM = True 
+                       afterMcount = 2                   
                 elif char == 'z':
                     letter += 1 
                     INDEX = '58C'
@@ -152,6 +187,7 @@ def gen():
                         f.write(f'\tdc.w $00{width[char]}, $8{INDEX}, $8{INDEX2P}, ${XPOS} ; {char.upper()} \n' )               
                 elif char == ' ':
                     print('')
+                    cur_pos += 16                   
                     if export == True:
                         f.write('\n')
             
